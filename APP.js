a031bcf9aa700ee106435a25b875bc285616f1c2
Saketh-code-rgb <yetinthalasaketh@gmail.com>
2025-07-31 12:36:44 +0530

    Create APP.js

---
+++ APP.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
